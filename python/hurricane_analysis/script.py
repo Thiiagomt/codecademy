d43c1ce4edd23eb83067550c833c6b836411b097
Thiago <tmoraes505@gmail.com>
2020-07-05 20:29:13 -0300

    missing the 3 last functions

--- python/hurricane_analysis/script.py
+++ python/hurricane_analysis/script.py
@@ -20,65 +20,128 @@ damages = ['Damages not recorded', '100M', 'Damages not recorded', '40M', '27.9M
 deaths = [90,4000,16,3103,179,184,408,682,5,1023,43,319,688,259,37,11,2068,269,318,107,65,19325,51,124,17,1836,125,87,45,133,603,138,3057,74]
 
 # write your update damages function here:
+def update_damages(list_damages):
+    new_damages = []
 
+    for damage in list_damages:
+        if damage == "Damages not recorded":
+            new_damages.append(damage)
+        elif damage[-1] == "M":
+            new_damage = float(damage[:len(damage)-1]) * 1000000.0
+            new_damages.append(new_damage)
+        else:
+            new_damage = float(damage[:len(damage)-1]) * 1000000000.0
+            new_damages.append(new_damage)
 
+    return new_damages
 
-
-
-
+new_damages = update_damages(damages)
+print()
+print(new_damages)
+print()
 
 # write your construct hurricane dictionary function here:
+def hurricane_dictionary(names, months, years, max_sustained_winds, areas_affected, damages, deaths):
+    dictionary = {}
+    i = 0
 
+    for name in names:
+        dictionary[name] = {
+            'Name':names[i],
+            'Month':months[i],
+            'Year':years[i],
+            'Max Sustained Wind':max_sustained_winds[i],
+            'Areas Affected':areas_affected[i],
+            'Damage':damages[i],
+            'Deaths':deaths[i]
+        }
+        i+=1
 
+    return dictionary
 
-
-
-
+main_dict = hurricane_dictionary(names, months, years, max_sustained_winds, areas_affected, new_damages, deaths)
+print(main_dict['Cuba I'])
+print()
 
 # write your construct hurricane by year dictionary function here:
+def years_dictionary(main_dict):
+    new_dict = {}
+    for hurricane in main_dict:
+        current_year = main_dict[hurricane]['Year']
+        if current_year in new_dict:
+            new_dict[current_year].append(main_dict[hurricane])
+        else:
+            new_dict[current_year] = [main_dict[hurricane]]
 
+    return new_dict
 
-
-
-
-
+years_dict = years_dictionary(main_dict)
+print(years_dict[1932])
+print()
 
 # write your count affected areas function here:
+def areas_dictionary(main_dict):
+    new_dict = {}
 
+    for hurricane in main_dict:
+        current_area = main_dict[hurricane]['Areas Affected']
+        for area in current_area:
+            if area in new_dict:
+                new_dict[area] += 1
+            else:
+                new_dict[area] = 1
 
+    return new_dict
 
-
-
-
+areas_dict = areas_dictionary(main_dict)
+print(areas_dict)
+print()
 
 # write your find most affected area function here:
+def most_affected_area(areas_dict):
+    max_times = 0
+    affected_area = ""
 
+    for area in areas_dict:
+        if max_times < areas_dict[area]:
+            max_times = areas_dict[area]
+            affected_area = area
 
+    return affected_area, max_times
 
-
-
-
+most_affect_area = most_affected_area(areas_dict)
+print('The most affected area is: ' + str(most_affect_area[0] + ', with ' + str(most_affect_area[1]) + ' huricanes attacks.'))
+print()
 
 # write your greatest number of deaths function here:
+def most_deaths(main_dict):
+    max_deaths = 0
+    hurricane_name = ""
 
+    for hurricane in main_dict:
+        if max_deaths < main_dict[hurricane]['Deaths']:
+            max_deaths = main_dict[hurricane]['Deaths']
+            hurricane_name = main_dict[hurricane]['Name']
 
+    return hurricane_name, max_deaths
 
+most_deaths_hurricane = most_deaths(main_dict)
+print('The greatest number of deaths was on ' + str(most_deaths_hurricane[0]) + ', with ' + str(most_deaths_hurricane[1]) + ' deaths.')
+print()
 
-
-
-
-# write your catgeorize by mortality function here:
-
-
-
+# write your categorize by mortality function here:
+mortality_scale = {0: 0,
+                   1: 100,
+                   2: 500,
+                   3: 1000,
+                   4: 10000}
+def mortality_scale_dictionary(main_dict, mortality_scale):
+    new_dict = {0:[], 1:[], 2:[], 3:[], 4:[], 5:[]}
 
 
 
 
 # write your greatest damage function here:
-
-
-
 
 
 
