17981930aae0f62a9f1269e6763796dd44b1e654
Thiago <tmoraes505@gmail.com>
2020-07-16 17:43:44 -0300

    final basic version

--- java/best_fare_calculator/src/TransitCalculator.java
+++ java/best_fare_calculator/src/TransitCalculator.java
@@ -11,25 +11,75 @@ public class TransitCalculator {
     String[] fareOptions = {"Pay-per-ride", "7-day Unlimited rides", "30-day Unlimited rides"};
     double[] farePrices = {2.75, 33, 127};
 
-    public TransitCalculator(int days, int rides){
+    public TransitCalculator(int rides, int days){
         daysOfUse = days;
         individualRides = rides;
     }
 
-    public double unlimited7Price(int days, int rides) {
-        int weeks = days / 7;
+    // Return price/ride on 7-day Unlimited rides fare
+    public double unlimited7Price() {
+        int weeks = daysOfUse / 7;
 
         double totalFare;
 
-        if (days % 7 == 0) {
+        if (daysOfUse % 7 == 0) {
             totalFare = weeks * farePrices[1];
         } else {
             totalFare = (weeks + 1) * farePrices[1];
         }
 
-        return totalFare / rides;
+        return totalFare / individualRides;
     }
 
+    // Return price/ride on 30-day Unlimited rides fare
+    public double unlimited30Price() {
+        int weeks = daysOfUse / 30;
+
+        double totalFare;
+
+        if (daysOfUse % 30 == 0) {
+            totalFare = weeks * farePrices[2];
+        } else {
+            totalFare = (weeks + 1) * farePrices[2];
+        }
+
+        return totalFare / individualRides;
+    }
+
+    // Return array of doubles of price/ride for each fare option
+    public double[] getRidePrices() {
+
+        double[] fares = new double[3];
+
+        double payPerRideFare = farePrices[0];
+        double unlimited7DaysFare = unlimited7Price();
+        double unlimited30DaysFare = unlimited30Price();
+
+        fares[0] = payPerRideFare;
+        fares[1] = unlimited7DaysFare;
+        fares[2] = unlimited30DaysFare;
+
+        return fares;
+    }
+
+    // Return a advice to pick the best fare for rides/days
+    public String getBestFare(){
+        double[] arrayPrices = getRidePrices();
+        double lowestPrice = arrayPrices[0];
+        int index = 0;
+
+        for(int i=0; i<3; i++){
+            if(arrayPrices[i] < lowestPrice){
+                lowestPrice = arrayPrices[i];
+                index = i;
+            }
+        }
+
+        // Format lowestPrice to print
+        lowestPrice = Math.round(lowestPrice * 100.0) / 100.0;
+
+        return "You should get the " + fareOptions[index] + " option at $" + lowestPrice + " per ride.";
+    }
 
     public String toString(){
         return "Vai da o cu " + daysOfUse + " horas";
@@ -37,8 +87,7 @@ public class TransitCalculator {
 
     public static void main(String[] args){
 
-
-        TransitCalculator object = new TransitCalculator(30, 50);
-        System.out.println(object.unlimited7Price(6, 14));
+        TransitCalculator object = new TransitCalculator(54, 26);
+        System.out.println(object.getBestFare());
     }
 }
